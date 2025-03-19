7d7a15d870f9b95bf6aa5ae8430cab6d54030528
MNuenninghoff <mikenue@gmail.com>
2025-03-18 19:21:53 -0500

    created gitignore file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,16 +1,12 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 puts 'Event Manager Initialized!'
 
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = File.read('secret.key').strip
+
 def clean_zipcode(zipcode)
-  if zipcode.nil?
-    zipcode = '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  else
-    zipcode
-  end
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 contents = CSV.open(
@@ -21,7 +17,17 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
+  begin     
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+  rescue 
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 
   
-  puts "#{name} #{zipcode}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
